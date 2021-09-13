12bfb00f07260abe88b4a7a3cca97a2fac47a31f
lucas <lucasportellaagu@gmail.com>
2021-09-13 13:27:27 -0300

    fix status codes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,10 +1,11 @@
+const { StatusCodes } = require('http-status-codes');
 const servicesUsers = require('../services/users');
 
 const createUser = async (req, res) => {
     const { name, email, password } = req.body;
     const userData = { name, email, password };
     const result = await servicesUsers.createUser(userData);
-    return res.status(200).json(result);
+    return res.status(StatusCodes.CREATED).json(result);
 };
 
 module.exports = {

cc0e5fe12d2ba7a2753d053dea750fc66b637aae
lucas <lucasportellaagu@gmail.com>
2021-09-13 18:29:23 -0300

    req 2

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -4,6 +4,12 @@ const servicesLogin = require('../services/login');
 const makeLogin = async (req, res) => {
     const { email, password } = req.body;
     const result = await servicesLogin.makeLogin(email, password);
+    if (result.errorType === 'unauthorized - empty field') {
+        return res.status(StatusCodes.UNAUTHORIZED).json(result.error);
+    }
+    if (result.errorType === 'unauthorized incorrect data') {
+        return res.status(StatusCodes.UNAUTHORIZED).json(result.error); 
+}
     return res.status(StatusCodes.OK).json(req.token);
 };
 
--- src/services/login.js
+++ src/services/login.js
@@ -1,7 +1,18 @@
 const modelsLogin = require('../models/login');
 
 const makeLogin = async (email, password) => {
+    if (!email || !password) {
+        return { errorType: 'unauthorized - empty field',
+         error: { message: 'All fields must be filled' } };
+    }
+
     const result = await modelsLogin.makeLogin(email, password);
+
+    if (!result) {
+        return { errorType: 'unauthorized incorrect data',
+         error: { message: 'Incorrect username or password' } };
+    }
+
     return result;
 };
 

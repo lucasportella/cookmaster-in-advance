b2c037afe1c7a53d45ae3996ec196cb388c392c4
lucas <lucasportellaagu@gmail.com>
2021-09-14 18:24:59 -0300

    seed query and function req 6 done

---
+++ src/models/admin.js
@@ -0,0 +1,11 @@
+const connection = require('./connection');
+
+const adminSeed = async () => {
+    const admin = { name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' };
+    const db = await connection();
+    await db.collection('users').insertOne(admin);
+};
+
+module.exports = {
+    adminSeed,
+};

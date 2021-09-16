16441a8f60c00bc39b8f6237e80624588c1e9a42
lucas <lucasportellaagu@gmail.com>
2021-09-16 17:12:50 -0300

    fix jwt security flaw

--- src/middlewares/jsonWebToken.js
+++ src/middlewares/jsonWebToken.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const generateJwtToken = async (req, res, next) => {
-    const { email, password } = req.body;
+    
+    const { email, name } = req.body;
     const secret = 'mysecretexample';
     const jwtConfig = {
         expiresIn: '7d',
         algorithm: 'HS256',
     };
-    const token = jwt.sign({ email, password }, secret, jwtConfig);
+    const token = jwt.sign({ email, name }, secret, jwtConfig);
 
     req.token = { token };
 

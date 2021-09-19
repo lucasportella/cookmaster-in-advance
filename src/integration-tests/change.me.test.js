421ee5d598d9dc46a78cea33fe56ddf83125a98e
lucas <lucasportellaagu@gmail.com>
2021-09-19 12:38:44 -0300

    more tests

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -10,7 +10,7 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe('', () => {
+describe('get recipes', () => {
     let response = {};
     const DBServer = new MongoMemoryServer();
 
@@ -19,10 +19,19 @@ describe('', () => {
         const connectionMock = await MongoClient.connect(URLMock, {useNewUrlParser: true, useUnifiedTopology: true})
         sinon.stub(MongoClient, 'connect').resolves(connectionMock)
 
-        // const user = { "name" : "Erick Jacquin", "email" : "erickjacquin@gmail.com", "password" : "12345678", "role" : "user" }
+        emptyRecipes = await chai.request(server).get('/recipes')
 
-        // response = await chai.request(server).post('/users').send(user);
-        response = await chai.request(server).get('/recipes')
+        const user = { "name" : "Erick Jacquin", "email" : "erickjacquin@gmail.com", "password" : "12345678", "role" : "user" }
+
+        const login = {"email" : "erickjacquin@gmail.com", "password" : "12345678"}
+
+        const recipe = { "name" : "Receita do Jacquin", "ingredients" : "Frango", "preparation" : "10 minutos no forno" }
+
+        newUser = await chai.request(server).post('/users').send(user);
+
+        token = await chai.request(server).post('/login').send(login)
+
+        newRecipe = await chai.request(server).post('/recipes').set('authorization', token.body.token).send(recipe)
 
     })
     
@@ -31,12 +40,35 @@ describe('', () => {
         await DBServer.stop();
     })
     
+    // empty recipes
     it('return status 200', async () => {
-        expect(response).to.have.status(200)
+        expect(emptyRecipes).to.have.status(200)
     })
 
     it('return an empty array', async () => {
-        expect(response.body).to.be.a('array')
-        expect(response.body.length).to.equal(0)
+        expect(emptyRecipes.body).to.be.a('array')
+        expect(emptyRecipes.body.length).to.equal(0)
+    })
+
+    // create new user
+    it('returns status 201', async () => {
+        expect(newUser).to.have.status(201)
+    })
+
+    it('returns an object', async () => {
+        expect(newUser).to.be.a('object')
+        expect(newUser.body.user).to.have.any.keys('name', 'email', 'role', '_id')
+    })
+
+    // login
+    it('when login is successful, returns token', async () => {
+        expect(token).to.have.status(200)
+        expect(token.body.token).to.be.a('string')
+    })
+
+    // post new recipe
+    it('creates new recipe on db', async () => {
+        expect(newRecipe).to.have.status(201)
+        expect(newRecipe.body.recipe).to.have.all.keys('name', 'ingredients', 'preparation', 'userId', '_id')
     })
 });

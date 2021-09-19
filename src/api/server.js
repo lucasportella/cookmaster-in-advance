cd9bddc8d3b2cf633da01a5dfead190f6257c695
lucas <lucasportellaagu@gmail.com>
2021-09-19 11:53:15 -0300

    integration tests - beginning

--- src/api/server.js
+++ src/api/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
+module.exports = app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,2 +1,42 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const server = require('../api/server');
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
 describe('', () => {
+    let response = {};
+    const DBServer = new MongoMemoryServer();
+
+    before( async () => {
+        const URLMock = await DBServer.getUri();
+        const connectionMock = await MongoClient.connect(URLMock, {useNewUrlParser: true, useUnifiedTopology: true})
+        sinon.stub(MongoClient, 'connect').resolves(connectionMock)
+
+        // const user = { "name" : "Erick Jacquin", "email" : "erickjacquin@gmail.com", "password" : "12345678", "role" : "user" }
+
+        // response = await chai.request(server).post('/users').send(user);
+        response = await chai.request(server).get('/recipes')
+
+    })
+    
+    after(async () => {
+        MongoClient.connect.restore();
+        await DBServer.stop();
+    })
+    
+    it('return status 200', async () => {
+        expect(response).to.have.status(200)
+    })
+
+    it('return an empty array', async () => {
+        expect(response.body).to.be.a('array')
+        expect(response.body.length).to.equal(0)
+    })
 });

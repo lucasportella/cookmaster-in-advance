2485fba6ae1db8c0ee73496c6ebf277b654a2814
lucas <lucasportellaagu@gmail.com>
2021-09-20 19:02:59 -0300

    dotenv

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 module.exports = app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
